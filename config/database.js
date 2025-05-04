0c32878a82e8eb7afdd1e73091dde06be18cc4d0
Osman Ciftci <oaciftci@hotmail.com>
2025-05-04 02:31:24 +0200

    enabled ssl database connection after error on render.

--- config/database.js
+++ config/database.js
@@ -8,6 +8,7 @@ module.exports = ({ env }) => ({
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
       // ssl: env.bool('DATABASE_SSL'),
+      ssl: env.bool('DATABASE_SSL', false) ? { rejectUnauthorized: false } : false,
       acquireTimeoutMillis: 300000,
       pool: {
         min: 0,  // default 2
